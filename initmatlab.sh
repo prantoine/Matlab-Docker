0854865236f00248d24d96d71ae2325d7d385364
Pol Antoine <pol.antoine@sciencespo.fr>
2026-03-19 18:20:19 +0100

    Update init script to pass credentials as secrets

--- initmatlab.sh
+++ initmatlab.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
 mc cp s3/prantoine/matlab.yaml ~/work/matlab.yaml
 
-#this env variable is necessary to use command 'mc' within the container. the user becomes authenticated with s3 service.
-export MC_HOST_s3="https://${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY}:${AWS_SESSION_TOKEN}@${AWS_S3_ENDPOINT}"
+#this file contains credentials generated via apache2 'htpasswd' tool.
+mc cp s3/prantoine/auth ~/work/auth
+
+#this bash variable holds the secret required for 'mc' to work within the container.
+#it is passed to the container through the 'create secret' command from kubectl.
+MC_HOST_s3="https://${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY}:${AWS_SESSION_TOKEN}@${AWS_S3_ENDPOINT}"
+kubectl create secret generic mc-host-secret-name --from-literal=mc-host-secret-key="${MC_HOST_s3}"
+
+#add the authentication file as a secret.
+kubectl create secret generic basic-auth --from-file=~/work/auth
 
 kubectl apply -f ~/work/matlab.yaml
